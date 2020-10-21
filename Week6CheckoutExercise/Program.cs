4f638eb79fb29d21b8cc7df3e121b72419003f4c
Thomas Lancrenon <23199@student.dorset-college.ie>
2020-10-21 15:11:09 +0100

    Ajoutez des fichiers projet.

---
+++ Week6CheckoutExercise/Program.cs
@@ -0,0 +1,16 @@
+﻿
+
+
+
+using System;
+
+namespace Week6CheckoutExercise
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
